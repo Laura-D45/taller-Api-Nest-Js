5dc20d03e58862f0e0c80aee2f891bd5a89333af
Laura <lauradiaz.business42@gmail.com>
2025-10-02 22:33:51 -0500

    Implementacion de controladores y en pedido y producto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductoModule } from './producto/producto.module';
+import { PedidoModule } from './pedido/pedido.module';
+
 
 @Module({
-  imports: [ProductoModule],
+  imports: [ProductoModule, PedidoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProductoController } from './producto.controller';
+import { ProductoService } from './producto.service';
 
-@Module({})
+@Module({
+  controllers: [ProductoController],
+  providers: [ProductoService]
+})
 export class ProductoModule {}
